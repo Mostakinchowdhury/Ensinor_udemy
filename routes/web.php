b383dcf36513da80c272f619e34ed5f15713e4f6
Mostakin chowdhury <chowdhurymostakin02@gmail.com>
2026-04-21 19:05:15 +0600

    available all branch

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+use Inertia\Inertia;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
-});
-
-require __DIR__.'/settings.php';
+Route::inertia('/', 'Welcome');
+Route::get('/about', function () {
+    return Inertia::render('About');
+})->name('about');
