2f865a6b6adc975cbcb747812966b7f72297c2f8
tonmoystark <tonmoy.a009@gmail.com>
2026-04-27 18:36:03 +0600

    responsive the instructor cards

--- resources/js/components/Instructor/EveryInstructorDataContainer.tsx
+++ resources/js/components/Instructor/EveryInstructorDataContainer.tsx
@@ -6,17 +6,17 @@ const EveryInstructorDataContainer = () => {
   return (
     <div className='max-w-6xl mx-auto'>
         <FilterInstructor />
-        <div className='grid grid-cols-3 gap-4'>
-          <InstructorCard />
-          <InstructorCard />
-          <InstructorCard />
-          <InstructorCard />
-          <InstructorCard />
-          <InstructorCard />
-          <InstructorCard />
-          <InstructorCard />
-          <InstructorCard />
-        </div>
+        <div className="grid grid-cols-1 mx-4 sm:mx-0 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+  <InstructorCard />
+  <InstructorCard />
+  <InstructorCard />
+  <InstructorCard />
+  <InstructorCard />
+  <InstructorCard />
+  <InstructorCard />
+  <InstructorCard />
+  <InstructorCard />
+</div>
     </div>
   )
 }
