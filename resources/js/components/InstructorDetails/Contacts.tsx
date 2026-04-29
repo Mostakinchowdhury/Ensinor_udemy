4897a615a31834c3a55872eb45468521345840fd
tonmoystark <tonmoy.a009@gmail.com>
2026-04-29 17:52:43 +0600

    fixed a mistake

--- resources/js/components/InstructorDetails/Contacts.tsx
+++ resources/js/components/InstructorDetails/Contacts.tsx
@@ -2,22 +2,7 @@ import React from 'react';
 
 const Contacts = () => {
     return (
-        <div className="my-10">
-            <p className='text-slate-600'>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi
-                atque, asperiores neque, ipsam inventore culpa maxime facilis
-                vel reprehenderit accusamus, voluptates consequuntur fuga iste!
-                Fugiat distinctio a aliquam iure, soluta quis repellat dolorem
-                dolor sapiente? Eveniet, nesciunt repudiandae? Similique nisi
-                officia ipsam quos fugit excepturi veritatis culpa dolores
-                delectus est.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Tempore, harum. Non, modi? Officiis neque non ratione in
-                voluptates? Voluptatum, reiciendis.
-            </p>
-        </div>
+        <div>tonmoy</div>
     );
 };
 
--- resources/js/components/InstructorDetails/PersonalData.tsx
+++ resources/js/components/InstructorDetails/PersonalData.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 const PersonalData = () => {
-  return (
-    <div>
-        <h1 className='text-2xl font-bold'>Hafizur Rahman</h1>
-        <p className='text-sm text-slate-600'>Full Stack Web Developer</p>
-    </div>
-  )
-}
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Hafizur Rahman</h1>
+            <p className="text-sm text-slate-600">Full Stack Web Developer</p>
+            <div className="my-10">
+                <p className="text-slate-600">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Quasi atque, asperiores neque, ipsam inventore culpa maxime
+                    facilis vel reprehenderit accusamus, voluptates consequuntur
+                    fuga iste! Fugiat distinctio a aliquam iure, soluta quis
+                    repellat dolorem dolor sapiente? Eveniet, nesciunt
+                    repudiandae? Similique nisi officia ipsam quos fugit
+                    excepturi veritatis culpa dolores delectus est.
+                    <br />
+                    <br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tempore, harum. Non, modi? Officiis neque non ratione in
+                    voluptates? Voluptatum, reiciendis.
+                </p>
+            </div>
+        </div>
+    );
+};
 
-export default PersonalData
+export default PersonalData;
