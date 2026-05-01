64b39b6ddf892fcfbe6f09d27b357596a54d526b
tonmoystark <tonmoy.a009@gmail.com>
2026-05-01 18:40:05 +0600

    instructordetail only profilecard refined

--- resources/js/components/InstructorDetails/ProfileCard.tsx
+++ resources/js/components/InstructorDetails/ProfileCard.tsx
@@ -6,7 +6,7 @@ const ProfileCard = () => {
         <div className='bg-[#f3f6f7] w-78 h-103 p-4 rounded-2xl shadow-md hover:shadow-xl transition duration-300 flex flex-col items-center justify-between'>
             
             {/* Image */}
-<div className="w-full h-70 rounded-xl overflow-hidden shadow">
+<div className="w-[280px] h-[302px] rounded-xl overflow-hidden shadow">
     <img 
         src="./contributer.png" 
         alt="profile" 
@@ -15,30 +15,22 @@ const ProfileCard = () => {
 </div>
 
             {/* Rating */}
-            <div className="flex items-center gap-1">
-                <p className="text-gray-500 text-sm">Ratings</p>
-                <p className="flex items-center gap-1 text-lg font-semibold text-yellow-500">
+            <div className="flex items-center text-[16px] text-text50 gap-5">
+                <p>Ratings</p>
+                <p className="flex items-center gap-1">
                     <span>⭐</span>
-                    <span className="text-gray-800">4.6/5.0</span>
+                    <span className="text-gray-800">4.6 / 5.0</span>
                 </p>
             </div>
 
             {/* Follow */}
-            <div className='flex  items-center gap-2'>
-                <p className="text-gray-500 text-sm">Follow</p>
+            <div className='flex  items-center gap-4 text-[16px] text-text50'>
+                <p>Follow</p>
                 <div className="flex gap-3">
-                    <span className="p-2 bg-white rounded-full shadow hover:bg-blue-500 hover:text-white transition cursor-pointer">
-                        <Facebook size={18} />
-                    </span>
-                    <span className="p-2 bg-white rounded-full shadow hover:bg-blue-700 hover:text-white transition cursor-pointer">
-                        <Linkedin size={18} />
-                    </span>
-                    <span className="p-2 bg-white rounded-full shadow hover:bg-sky-400 hover:text-white transition cursor-pointer">
-                        <Twitter size={18} />
-                    </span>
-                    <span className="p-2 bg-white rounded-full shadow hover:bg-pink-500 hover:text-white transition cursor-pointer">
-                        <Instagram size={18} />
-                    </span>
+                    <Facebook className='hover:text-blue-600' size={20} />
+                    <Linkedin className='hover:text-blue-600' size={20} />
+                    <Twitter className='hover:text-blue-400' size={20} />
+                    <Instagram className='hover:text-pink-600' size={20} />
                 </div>
             </div>
 
